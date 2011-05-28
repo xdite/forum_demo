3179d11e071e39d60c4db16a9e4cf0b0cb72673f
xdite <xdite@techbang.com.tw>
2011-05-29 07:18:51 +0800

    post must have user_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :board
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110528224249) do
+ActiveRecord::Schema.define(:version => 20110528231533) do
 
   create_table "boards", :force => true do |t|
     t.string   "name"
