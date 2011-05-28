994fec9d09302f03215b0131a4e802dc120d7388
xdite <xdite@techbang.com.tw>
2011-05-29 06:52:16 +0800

    change root to boards#index

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ ForumDemo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "pages#welcome"
+  root :to => "boards#index"
 
   # See how all your routes lay out with "rake routes"
 
