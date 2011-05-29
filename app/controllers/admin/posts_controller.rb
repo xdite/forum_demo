037a32e5446ebdc9f0b55109aaf9bd8d2f1788a8
xdite <xdite@techbang.com.tw>
2011-05-29 09:02:34 +0800

    due to deplicate
    remove admin/posts#show
    remove admin/posts#new
    remive admin/posts#index
    
    add 'A-' to admin-only action

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,65 +2,18 @@ class Admin::PostsController < ApplicationController
   layout 'admin'
     
   before_filter :find_board
-  before_filter :authenticate_user! , :except => [ :show, :index ]
-  # GET /posts
-  # GET /posts.xml
-  def index
-    redirect_to board_path(@board)
-  end
-
-  # GET /posts/1
-  # GET /posts/1.xml
-  def show
-    
-    @post = @board.posts.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
-  end
-
-  # GET /posts/new
-  # GET /posts/new.xml
-  def new
-
-    @post = @board.posts.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
-  end
+  before_filter :authenticate_user! , :except => [ :index ]
 
   # GET /posts/1/edit
   def edit
-    @post = current_user.posts.find(params[:id])
-  end
-
-  # POST /posts
-  # POST /posts.xml
-  def create
-    
-    @post = @board.posts.build(params[:post])
-    @post.user_id = current_user.id
-    
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to(board_post_path(@board,@post), :notice => 'Post was successfully created.') }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
-    end
+    @post = Post.find(params[:id])
   end
 
   # PUT /posts/1
   # PUT /posts/1.xml
   def update
     
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
 
     respond_to do |format|
       if @post.update_attributes(params[:post])
@@ -77,7 +30,7 @@ class Admin::PostsController < ApplicationController
   # DELETE /posts/1.xml
   def destroy
     
-    @post = current_user.posts.find(params[:id])
+    @post = Post.find(params[:id])
     @post.destroy
 
     respond_to do |format|
