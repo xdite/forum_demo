fdfefb2b73e10b7bfc04028ab2b07c0cceb58683
xdite <xuite.joke@gmail.com>
2011-05-30 01:19:32 +0800

    recent scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :board, :counter_cache => true
   belongs_to :user
   
-  default_scope :order => 'created_at DESC'
-  
+  #default_scope :order => 'created_at DESC'
+  scope :recent, :order => 'updated_at DESC'
 end
