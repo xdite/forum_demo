71f7c3957fe85c3aad4cb6c6d74896e32d3f6fdb
xdite <xdite@techbang.com.tw>
2011-06-11 02:21:42 +0800

    write seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+admin = User.new(:email => "admin@example.org", :password => "123456", :password_confirmation => "123456")
+admin.is_admin = true
+admin.save!
+
+normal_user = User.new(:email => "user1@example.org", :password => "123456", :password_confirmation => "123456")
+normal_user.save!
+
+board = Board.create!(:name => "System Announcent")
+
+post = board.posts.build(:title => "First Post", :content => "This is a demo post")
+post.user = admin
+post.save!
+
